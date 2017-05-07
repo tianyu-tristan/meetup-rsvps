6aa816dfa2d0182815fd1df296f845687a120815
tristan <tianyu.tristan@gmail.com>
2017-05-06 22:26:41 -0700

    changed home

--- code/kafka/install_spark.sh
+++ code/kafka/install_spark.sh
@@ -2,7 +2,7 @@
 wget http://d3kbcqa49mib13.cloudfront.net/spark-2.1.0-bin-hadoop2.7.tgz
 tar xvf spark-2.1.0-bin-hadoop2.7.tgz
 sudo mv spark-2.1.0-bin-hadoop2.7 /usr/local/spark
-echo 'export SPARK_HOME=/usr/local/spark' >> ~/.bashrc
-echo 'export PYSPARK_PYTHON=/usr/bin/python3' >> ~/.bashrc
-echo 'export PATH=$SPARK_HOME/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
+echo 'export SPARK_HOME=/usr/local/spark' >> /home/ubuntu/.bashrc
+echo 'export PYSPARK_PYTHON=/usr/bin/python3' >> /home/ubuntu/.bashrc
+echo 'export PATH=$SPARK_HOME/bin:$PATH' >> /home/ubuntu/.bashrc
+source /home/ubuntu/.bashrc
